2e432394780ce5aa7c79e3b120a41ca6cfcce435
Abduljalil Abiri <66950832+abdulj007@users.noreply.github.com>
2022-02-12 11:20:11 +0100

    Overall volume growth till date
    This insight shows  overall volume growth by overall session and order volume, trended by
     quarter for the life of the business.

---
+++ Overall volume growth.sql
@@ -0,0 +1,15 @@
+USE mavenfuzzyfactory;
+
+/*1. Show overll volume growth. Pull overall session and order volume, trended by
+ quarter for the life of the business. */
+
+SELECT 
+	YEAR (website_sessions.created_at) AS yr, 
+    QUARTER (website_sessions.created_at) AS qtr, 
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions, 
+    COUNT(DISTINCT orders.order_id) AS orders
+FROM website_sessions
+	LEFT JOIN orders
+		ON website_sessions.website_session_id = orders.website_session_id
+GROUP BY 1, 2
+ORDER BY 1 ,2; 
